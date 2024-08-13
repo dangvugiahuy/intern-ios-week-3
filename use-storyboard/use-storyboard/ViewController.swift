3be289a53d40f49fa96ad53abac25701271893ca
Gia Huy <dangvugiahuy.2002@gmail.com>
2024-08-13 11:39:00 +0700

    Project using storyboard

---
+++ use-storyboard/use-storyboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  use-storyboard
+//
+//  Created by huy.dang on 8/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
