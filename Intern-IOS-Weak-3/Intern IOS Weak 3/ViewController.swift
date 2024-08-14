01614a66977b9bd28e47b5b20c369fb4ce93488b
Gia Huy <dangvugiahuy.2002@gmail.com>
2024-08-14 16:58:31 +0700

    ReInit Project Intern IOS Week 3 from Mentor's Requiment

---
+++ Intern-IOS-Weak-3/Intern IOS Weak 3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Intern IOS Weak 3
+//
+//  Created by huy.dang on 8/14/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
